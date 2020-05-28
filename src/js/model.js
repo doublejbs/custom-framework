518e40fe13a214d7f66d54232b42a590f1834011
doublejbs <doublejbs@naver.com>
2020-05-28 11:59:42 +0900

    마무리 작업

--- src/js/model.js
+++ src/js/model.js
@@ -1,10 +1,7 @@
 import _ from "lodash";
-const predefinedProps = ["hours", "minutes", "seconds"];
 
 export default class Model {
   constructor(callback) {
-    this.indexes = [];
-
     const proxy = new Proxy(this, {
       get(target, property) {
         return target[property];
@@ -12,16 +9,12 @@ export default class Model {
       set(target, property, value) {
         const oldValue = target[property];
 
-        if (this.indexes === undefined) this.indexes = [];
         if (Array.isArray(value)) {
-          if (target["containers"] === undefined)
+          if (!target["containers"])
             target["containers"] = { [property]: value };
           else target["containers"][property] = value;
-
-          this.indexes.push(property);
         } else target[property] = value;
-        console.log("target", target);
-        // Notify model changes if value is changed.
+
         if (!_.isEqual(value, oldValue) && callback) {
           callback(property, oldValue, value);
         }
--- src/js/view.js
+++ src/js/view.js
@@ -4,6 +4,7 @@ import props from "snabbdom/es/modules/props";
 import Model from "./model";
 import { makeElement } from "./tools";
 import { makeParsedElement } from "./tools";
+import { v4 as uuidv4 } from "uuid";
 
 let itemStr = `<div id="item1" style=display:flex;flex-direction:row;padding:15px;justify-content:space-between; > <input type=text value=newTodo > <button id=remove innerText=- >`;
 let itemParse = `
@@ -26,33 +27,23 @@ export default class View {
     this.items = [];
   }
 
-  // add(items, model) {
-  //   return this.handleAdd(e, items, model);
-  // }
-
   add(items, model) {
     return function (e) {
       console.log("target clicked", e.target.id);
       if (e.target.id === "add") {
         const newItem = makeParsedElement(itemParse);
-        console.log("add new item", newItem);
-        console.log("type", typeof model["contents"]);
-        console.log("this", model);
-        // document.getElementById("contents").appendChild(newItem);
-        newItem.setAttribute("order", items.length);
+
+        newItem.setAttribute("id", uuidv4());
         items.push(newItem);
         model.contents = [...items];
       }
 
       if (e.target.id === "remove") {
-        console.log("order", e.target.parentNode.getAttribute("order"));
         const deleteItem = e.target.parentNode;
         items = items.filter(
-          (item) =>
-            item.getAttribute("order") !== deleteItem.getAttribute("order")
+          (item) => item.getAttribute("id") !== deleteItem.getAttribute("id")
         );
         model.contents = [...items];
-        // document.getElementById("contents").removeChild(deleteItem);
       }
     };
   }
@@ -67,6 +58,7 @@ export default class View {
 
     this.items = [];
     this.items.push(makeParsedElement(itemParse));
+    this.items[0].setAttribute("id", uuidv4());
     this.model.contents = this.items;
     // this.model.test = [];
 
@@ -85,14 +77,12 @@ export default class View {
     }
 
     this.requestRender = requestAnimationFrame(this.renderFunc);
+    console.log("render", this.requestRender);
   }
 
   render() {
     console.log("render!");
     const { containers } = this.model;
-    const { indexes } = this.model;
-
-    console.log("containers", indexes);
 
     const html = `
     <div id="wrapper" style="width: 100%;
@@ -132,14 +122,12 @@ box-shadow: inset 5px 5px 10px #d1cfc9,
 
         document.getElementById("todoContainer").appendChild(newCon);
       }
-      console.log("in", containers[key]);
+
       containers[key].forEach((elem) => {
         document.getElementById(key).appendChild(elem);
       });
     }
 
-    //this.container.innerHTML = `${hours}:${minutes}:${seconds}`;
     this.requestRender = 0;
-    console.log("render()");
   }
 }
